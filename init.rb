45a04b61e62a7c923b7eefd0b0a562ed50e6d1cf
cryptogopher <github@michalczyk.pro>
2016-12-05 21:00:13 +0100

    Plugin generated with: bin/rails g redmine_plugin bitcoin_voting

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :bitcoin_voting do
+  name 'Bitcoin Voting plugin'
+  author 'Piotr Michalczyk'
+  description 'Vote for Redmine ticket resolution with Bitcoin'
+  version '0.0.1'
+  url 'https://github.com/cryptogopher/bitcoin_voting'
+  author_url 'https://github.com/cryptogopher'
+end
