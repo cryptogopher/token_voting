754ee4e0c232bda37dc7fca33a6a382bec85f411
Piotr Michalczyk <github@michalczyk.pro>
2017-10-07 00:27:16 +0000

    Minor changes

--- app/models/bitcoin_vote.rb
+++ app/models/bitcoin_vote.rb
@@ -5,12 +5,12 @@ class BitcoinVote < ActiveRecord::Base
   belongs_to :user
 
   EXPIRATION_PERIODS = {
-    "week" => 1.week,
-    "month" => 1.month,
+    "1 week" => 1.week,
+    "1 month" => 1.month,
     "3 months" => 3.months,
     "6 months" => 6.months,
     "1 year" => 1.year,
-    "no expiration" => 99.years
+    "never" => 99.years
   }.freeze
 
   def initialize(args)
