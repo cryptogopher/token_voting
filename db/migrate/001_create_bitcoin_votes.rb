2ac5e3db816f30e80c07253af0b20f5a4b813db8
Piotr Michalczyk <github@michalczyk.pro>
2017-10-07 01:51:21 +0000

    Renaming db migration

--- db/migrate/001_create_bitcoin_votes.rb
+++
@@ -1,9 +0,0 @@
-class CreateTokenVotes < ActiveRecord::Migration
-  def change
-    create_table :token_votes do |t|
-      t.references :issue, index: true, null: false
-      t.references :user, index: true
-      t.datetime :expiration
-    end
-  end
-end
