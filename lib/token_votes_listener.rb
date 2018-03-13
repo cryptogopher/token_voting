e78ee79f1aa969182975b48ec240083f0807f811
Piotr Michalczyk <github@michalczyk.pro>
2018-03-13 18:52:45 +0000

    Corrected checkpoint table row numbering.

--- lib/token_votes_listener.rb
+++ lib/token_votes_listener.rb
@@ -1,5 +1,5 @@
 class TokenVotesListener < Redmine::Hook::Listener
   def controller_issues_edit_after_save(event)
-    TokenVote.issue_edit_hook(event[:issue])
+    TokenVote.issue_edit_hook(event[:issue], event[:journal])
   end
 end
