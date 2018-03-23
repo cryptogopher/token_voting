2aece4d66afaad76879f6a1d19f7330391938bfc
cryptogopher <github@michalczyk.pro>
2018-03-23 02:40:49 +0100

    Added plugin fixtures.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,8 @@
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+
+ActiveRecord::FixtureSet.create_fixtures(
+  File.dirname(__FILE__) + '/fixtures/',
+  [:issues, :token_votes, :token_payouts]
+)
+
--- test/unit/token_vote_test.rb
+++ test/unit/token_vote_test.rb
@@ -1,9 +1,11 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class TokenVoteTest < ActiveSupport::TestCase
+  fixtures :issues, :token_votes
 
   # Replace this with your real tests.
   def test_truth
     assert true
   end
 end
+
