acb64daf9ab01691bcf3004d18113388d998ef4c
cryptogopher <github@michalczyk.pro>
2018-05-12 22:50:07 +0200

    Test cleanup.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,6 @@
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
-#def id(sym)
-#  ActiveRecord::FixtureSet.identify(sym)
-#end
-
 ActiveRecord::FixtureSet.create_fixtures(File.dirname(__FILE__) + '/fixtures/',
   [
     :issues,
@@ -17,20 +13,24 @@ ActiveRecord::FixtureSet.create_fixtures(File.dirname(__FILE__) + '/fixtures/',
     :journal_details
   ])
 
-def after_issue_status_change(issue, user, status, &block)
+def update_issue_status(issue, user, status, &block)
   with_current_user user do
     journal = issue.init_journal(user)
     issue.status = status
     issue.save!
-    yield journal
+    TokenVote.issue_edit_hook(issue, journal)
     issue.clear_journal
   end
 end
 
 def TokenVote.generate!(attributes={})
   tv = TokenVote.new(attributes)
-  tv.voter ||= User.first
+  tv.voter ||= User.take
+  tv.issue ||= Issue.take
+  tv.duration ||= 1.month
+  tv.token ||= :BTCREG
   yield tv if block_given?
+  tv.generate_address
   tv.save!
   tv
 end
--- test/unit/token_vote_test.rb
+++ test/unit/token_vote_test.rb
@@ -16,40 +16,28 @@ class TokenVoteTest < ActiveSupport::TestCase
         'statuses' => [[issue_statuses(:resolved).id.to_s],
                      [issue_statuses(:pulled).id.to_s],
                      [issue_statuses(:closed).id.to_s]],
-        'shares' => ['0.1', '0.0', '0.9']
+        'shares' => ['0.7', '0.2', '0.1']
       }
     }
   end
 
   def test_3_checkpoints_forward_stepwise_processing_1_vote
     issue = issues(:issue_01)
-    #TokenVote.generate!( {issue: issue, expiration: 1.month, token: :BTCREG,
-    #                      amount_conf: 1.0} )
-    tv = TokenVote.new(voter: User.find_by_login(:bob),
-                       issue: issue,
-                       duration: 1.month,
-                       token: :BTCREG,
-                       amount_conf: 1.0)
-    tv.generate_address
-    tv.save!
+    TokenVote.generate!( {issue: issue, amount_conf: 1.0} )
 
     assert_no_difference 'TokenPayout.count' do
-      after_issue_status_change(issue, users(:alice), issue_statuses(:resolved)) do |journal|
-        TokenVote.issue_edit_hook(issue, journal)
-      end
+      update_issue_status(issue, users(:alice), issue_statuses(:resolved))
     end
     assert_no_difference 'TokenPayout.count' do
-      after_issue_status_change(issue, users(:bob), issue_statuses(:pulled)) do |journal|
-        TokenVote.issue_edit_hook(issue, journal)
-      end
+      update_issue_status(issue, users(:bob), issue_statuses(:pulled))
     end
-    assert_difference 'TokenPayout.count', 2 do
-      after_issue_status_change(issue, users(:charlie), issue_statuses(:closed)) do |journal|
-        TokenVote.issue_edit_hook(issue, journal)
-      end
+    assert_difference 'TokenPayout.count', 3 do
+      update_issue_status(issue, users(:charlie), issue_statuses(:closed))
     end
-    assert_equal TokenPayout.find_by(payee_id: users(:alice)).amount, 0.1
-    assert_equal TokenPayout.find_by(payee_id: users(:charlie)).amount, 0.9
+
+    assert_equal TokenPayout.find_by(payee_id: users(:alice)).amount, 0.7
+    assert_equal TokenPayout.find_by(payee_id: users(:bob)).amount, 0.2
+    assert_equal TokenPayout.find_by(payee_id: users(:charlie)).amount, 0.1
   end
 end
 
