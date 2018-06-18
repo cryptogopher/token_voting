c52c09d0856c8b6e361bb085a6e1e3d14dd472d4
cryptogopher <github@michalczyk.pro>
2018-06-18 15:00:45 +0200

    'My tokens' displays expirations and payouts on one tab.

--- lib/token_voting/my_controller_patch.rb
+++ lib/token_voting/my_controller_patch.rb
@@ -5,12 +5,12 @@ module TokenVoting
         { name: 'active',
           partial: 'my/token_votes/index',
           label: :label_active_votes },
+        { name: 'available',
+          partial: 'my/token_votes/available_tokens',
+          label: :label_available_tokens },
         { name: 'completed',
           partial: 'my/token_votes/index',
           label: :label_completed_votes },
-        { name: 'expired',
-          partial: 'my/token_votes/index',
-          label: :label_expired_votes },
         { name: 'withdrawals',
           partial: 'my/token_votes/index',
           label: :label_token_withdrawals },
@@ -18,7 +18,7 @@ module TokenVoting
 
       def token_votes
         @token_votes = Hash.new([])
-        [:active, :completed, :expired].each do |status|
+        [:active, :expired, :completed].each do |status|
           @token_votes[status.to_s] = TokenVote.where(voter: User.current).send(status)
         end
 
