f17ce493e42d902f20eb3d4e997733a3f2c46e90
cryptogopher <github@michalczyk.pro>
2018-09-13 01:10:40 +0200

    Added test_post_plugin_settings.

--- test/integration/settings_test.rb
+++ test/integration/settings_test.rb
@@ -7,6 +7,10 @@ class SettingsTest < Redmine::IntegrationTest
   def setup
     super
     setup_plugin
+
+    log_user 'alice', 'foo'
+    User.current.admin = true
+    User.current.save!
   end
 
   def teardown
@@ -15,15 +19,27 @@ class SettingsTest < Redmine::IntegrationTest
   end
 
   def test_get_plugin_settings
-    log_user 'alice', 'foo'
-    User.current.admin = true
-    User.current.save!
-
     get plugin_settings_path('token_voting')
     assert_response :ok
   end
 
   def test_post_plugin_settings
-    # TODO
+    post plugin_settings_path('token_voting'), {
+      settings: {
+        checkpoints: {
+          statuses: [issue_statuses(:pulled).id, '', issue_statuses(:closed).id, ''],
+          shares: [0.98, 0.02]
+        }
+      }
+    }
+    assert_redirected_to plugin_settings_path('token_voting')
+    assert_equal ["#{issue_statuses(:pulled).id}"],
+      Setting.plugin_token_voting['checkpoints']['statuses'].first
+    assert_equal ["#{issue_statuses(:closed).id}"],
+      Setting.plugin_token_voting['checkpoints']['statuses'].last
+    assert_equal "0.98", Setting.plugin_token_voting['checkpoints']['shares'].first
+    assert_equal "0.02", Setting.plugin_token_voting['checkpoints']['shares'].last
+    assert_equal 2, Setting.plugin_token_voting['checkpoints']['statuses'].length
+    assert_equal 2, Setting.plugin_token_voting['checkpoints']['statuses'].length
   end
 end
